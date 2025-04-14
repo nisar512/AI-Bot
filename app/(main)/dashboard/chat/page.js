91bc1753c7073955a07a24995f673375d54906eb
nisar512 <nisar.51297@gmail.com>
2025-04-15 02:38:01 +0500

    changes

--- app/(main)/dashboard/chat/page.js
+++ app/(main)/dashboard/chat/page.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const KnowledgePagepage = () => {
+const ChatPage = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const KnowledgePagepage = () => {
   )
 }
 
-export default KnowledgePagepage
+export default ChatPage
